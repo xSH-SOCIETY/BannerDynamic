807bd5d3dfba64e630761abec31441f4974556ee
xSH-SOCIETY <67087887+xSH-SOCIETY@users.noreply.github.com>
2020-06-18 02:11:36 -0300

    install.sh
    Este es el instalador de paquetes que necesitaremos para que todo salga bien.

---
+++ install.sh
@@ -0,0 +1,19 @@
+#!/Bin/Bash
+clear
+azulcielo='\e[1m\e[36m' #Colores
+echo -e "$amarillo INSTALANDO RECURSOS, ESPERE POR FAVOR"
+sleep 3
+apt install && apt upgrade
+sleep 1
+pkg install nano
+sleep 1
+apt install dialog -y
+sleep 1
+pkg install mpv -y
+sleep 1
+chmod +x BannerDynamic.sh
+chmod +x revertir.sh
+termux-setup-storage
+echo -e "$azulcielo   __  |_  .  _   _  | . _   _  |   | | ) (_| | | /_ (_)"  echo -e "$azulcielo Ahora ejecuta BannerDynamic.sh"
+sleep 3
+clear
